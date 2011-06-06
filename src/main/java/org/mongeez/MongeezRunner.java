079e18b2804c962995194b521b4ce908bdfa3657
OLEKSII IEPISHKIN <epishkin@gmail.com>
2011-06-06 17:18:58 -0400

    SMS-1098 change copyright and artifact version in pom.xml

--- src/main/java/org/mongeez/MongeezRunner.java
+++ src/main/java/org/mongeez/MongeezRunner.java
@@ -1,19 +1,13 @@
-/*************************************************************************
+/*
+ * Copyright 2011 SecondMarket Labs, LLC.
  *
- * RST GROUP CONFIDENTIAL
- * ______________________
- *
- * [2007] - [2011] RST Group Inc.
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains the property
- * of RST Group Inc. and its suppliers, if any. The intellectual and
- * technical concepts contained herein are proprietary to RST Group Inc.
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret and copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from RST Group Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
  */
 package org.mongeez;
 
