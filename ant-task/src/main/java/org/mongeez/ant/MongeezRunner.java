23f47b556a6d323e9d49418460c43bcbd22ae85e
Nick Lloyd <nicholas.lloyd@pearson.com>
2012-11-06 17:40:17 +0000

    code review-esque work
    - cleaned up unused variables and imports as well as simple eclipse
      reformatting

--- ant-task/src/main/java/org/mongeez/ant/MongeezRunner.java
+++ ant-task/src/main/java/org/mongeez/ant/MongeezRunner.java
@@ -10,78 +10,66 @@ import org.mongeez.MongoAuth;
 import org.springframework.core.io.FileSystemResource;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoOptions;
-import com.mongodb.ServerAddress;
 
 public class MongeezRunner extends Task {
     private String dbName;
-	private String host;
-	private String userName;
-	private String passWord;
-	private Integer port;
-	private String filePath;
-	private boolean writeSave = true;
+    private String host;
+    private String userName;
+    private String passWord;
+    private Integer port;
+    private String filePath;
 
     // The method executing the task
     public void execute() {
-    	try
-		{
-    	System.out.println("using following configs: dbName:"+ dbName +" host:" +host
-			+  " userName:" + userName + " passWord:" + passWord + " filePath:" + filePath 
-			+ " port:" + port);
-		if (StringUtils.isNotBlank(host) && StringUtils.isNotBlank(filePath))
-		{
-			
-			Mongeez mongeez = new Mongeez();
-			mongeez.setFile(new FileSystemResource(filePath));
-			try {
-				mongeez.setMongo(new Mongo(host, port));
-				if(StringUtils.isNotBlank(userName) && StringUtils.isNotBlank(passWord)){
-					MongoAuth auth = new MongoAuth(userName, passWord);
-					mongeez.setAuth(auth);
-				}
-			} catch (UnknownHostException e) {
-				throw new BuildException(e);
-			}
-			mongeez.setDbName(dbName);
-			mongeez.process();
-			
-		}
-		else
-		{
-			System.err.print("Host and FilePath is required");
-		}
-		}
-		catch (Exception e) {
-			System.err.print("Exception Occured: " + e.getMessage());
-			// TODO: handle exception in ant manner
-		}
+        try {
+            System.out.println("using following configs: dbName:" + dbName + " host:" + host + " userName:" + userName
+                    + " passWord:" + passWord + " filePath:" + filePath + " port:" + port);
+            if (StringUtils.isNotBlank(host) && StringUtils.isNotBlank(filePath)) {
+
+                Mongeez mongeez = new Mongeez();
+                mongeez.setFile(new FileSystemResource(filePath));
+                try {
+                    mongeez.setMongo(new Mongo(host, port));
+                    if (StringUtils.isNotBlank(userName) && StringUtils.isNotBlank(passWord)) {
+                        MongoAuth auth = new MongoAuth(userName, passWord);
+                        mongeez.setAuth(auth);
+                    }
+                } catch (UnknownHostException e) {
+                    throw new BuildException(e);
+                }
+                mongeez.setDbName(dbName);
+                mongeez.process();
+
+            } else {
+                System.err.print("Host and FilePath is required");
+            }
+        } catch (Exception e) {
+            System.err.print("Exception Occured: " + e.getMessage());
+            // TODO: handle exception in ant manner
+        }
     }
 
-	public void setDbName(String dbName) {
-		this.dbName = dbName;
-	}
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-	public void setPort(String port) {
-		this.port = port;
-	}
+    public void setPort(Integer port) {
+        this.port = port;
+    }
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
     public void setUserName(String userName) {
-		this.userName = userName;
-	}
+        this.userName = userName;
+    }
 
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
-	
-	public void setWriteSafe(boolean writeSafe) {
-	    this.writeSave = writeSafe;
-	}
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
