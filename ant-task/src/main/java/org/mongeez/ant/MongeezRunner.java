9a025d15acae141f917d70fcab077954ee86ae29
Nick Lloyd <nicholas.lloyd@pearson.com>
2012-11-15 15:52:29 +0000

    - replicaSet configurability now added to ant task

--- ant-task/src/main/java/org/mongeez/ant/MongeezRunner.java
+++ ant-task/src/main/java/org/mongeez/ant/MongeezRunner.java
@@ -1,6 +1,8 @@
 package org.mongeez.ant;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.tools.ant.BuildException;
@@ -10,10 +12,12 @@ import org.mongeez.MongoAuth;
 import org.springframework.core.io.FileSystemResource;
 
 import com.mongodb.Mongo;
+import com.mongodb.ServerAddress;
 
 public class MongeezRunner extends Task {
     private String dbName;
     private String host;
+    private String urls;
     private String username;
     private String password;
     private Integer port;
@@ -21,27 +25,51 @@ public class MongeezRunner extends Task {
 
     // The method executing the task
     public void execute() {
-        System.out.println("using following configs: dbName:" + dbName + " host:" + host + " username:" + username
-                + " password:" + password + " filePath:" + filePath + " port:" + port);
-        if (StringUtils.isNotBlank(host) && StringUtils.isNotBlank(filePath)) {
+        if (StringUtils.isNotBlank(filePath)) {
+            Mongo mongo = null;
+            try {
+                if(StringUtils.isNotBlank(host)) {
+                    System.out.println("Running mongeez against single mongodb instance...");
+                    System.out.println("using following configs: dbName:" + dbName + " host:" + host + " username:" + username
+                            + " password:" + password + " filePath:" + filePath + " port:" + port);
+                    mongo = new Mongo(host, port);
+                } else if(StringUtils.isNotBlank(urls)) {
+                    System.out.println("Running mongeez against replicaSet mongodb...");
+                    System.out.println("using following configs: dbName:" + dbName + " urls:" + urls + " username:" + username
+                            + " password:" + password + " filePath:" + filePath);
+                    List<ServerAddress> cluster = new ArrayList<ServerAddress>();
+                    String[] hostPortPairs = urls.split(",");
+                    if(hostPortPairs.length <= 1) {
+                        System.err.println("urls property MUST have more than one host:port pair, separated by commas");
+                        return;
+                    }
+                    for(String hostPortPair : hostPortPairs) {
+                        String[] hostPort = hostPortPair.split(":");
+                        if(hostPort.length != 2) {
+                            System.err.printf("%s in urls property is not of the format host:port!\n");
+                            return;
+                        }
+                        cluster.add(new ServerAddress(hostPort[0], Integer.valueOf(hostPort[1])));
+                    }
+                    mongo = new Mongo(cluster);
+                }
+            } catch(UnknownHostException e) {
+                throw new BuildException(e);
+            }
 
             Mongeez mongeez = new Mongeez();
             mongeez.setFile(new FileSystemResource(filePath));
-            try {
-                mongeez.setMongo(new Mongo(host, port));
-                if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
-                    MongoAuth auth = new MongoAuth(username, password);
-                    mongeez.setAuth(auth);
-                }
-            } catch (UnknownHostException e) {
-                throw new BuildException(e);
+            mongeez.setMongo(mongo);
+            if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+                MongoAuth auth = new MongoAuth(username, password);
+                mongeez.setAuth(auth);
             }
             mongeez.setDbName(dbName);
             mongeez.setVerbose(true);
             mongeez.process();
 
         } else {
-            System.err.print("Host and FilePath is required");
+            System.err.print("FilePath is required");
         }
     }
 
@@ -55,6 +83,10 @@ public class MongeezRunner extends Task {
 
     public void setPort(Integer port) {
         this.port = port;
+    }
+    
+    public void setUrls(String urls) {
+        this.urls = urls;
     }
 
     public void setFilePath(String filePath) {
